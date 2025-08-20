604a8d86fcb614ff91ebaa13bb0098a929b711c7
Rasmus <tonal_bada_9w@icloud.com>
2025-08-20 23:31:32 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,9 @@
+export const metadata = { title: "peradventure.se" }
 
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="sv">
+      <body>{children}</body>
+    </html>
+  )
+}
